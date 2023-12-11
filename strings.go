441a500cba58e9e8d3eaa78bca3a32fb16f9fdb6
keagan <sir.ellenberger@gmail.com>
2023-12-11 12:08:53 +0200

    feat: played with string formating

---
+++ strings.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main () {
+
+	age := 110
+	name := "aang"
+	element := "air"
+
+	//Print
+	fmt.Print("Hello, ")
+	fmt.Print("world! \n")
+	fmt.Print("new line \n")
+
+	//Print line
+	fmt.Println("greetings!")
+	fmt.Println("goodbye!")
+	fmt.Println("my name is:", name, "and my age is:", age)
+	
+	// Printf (formatted strings)
+	fmt.Printf("My age is %d and my age is %s I am an %sbender and the avatar \n", age, name, element)
+	fmt.Printf("age is of %T \n", age)
+	fmt.Printf("You scored %0.2f points! \n", 123.123)
+
+	//Sprintf saves string in variable for us
+	var statement string = fmt.Sprintf("My age is %d and my age is %s I am an %sbender and the avatar \n", age, name, element)
+	fmt.Println("The saved string:", statement)
+}
