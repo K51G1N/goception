989ccc74ae825c7e7aa7442c2db1d4c5d73c8f99
keagan <sir.ellenberger@gmail.com>
2023-12-13 12:35:16 +0200

    feature: package coupling

---
+++ projectscope.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+
+var points = []int{20, 30,40}
+
+func sayHello(n string) {
+	fmt.Printf("Hello %v \n", n)
+
+}
+
+func showScore() {
+	fmt.Println("You scored:", score)
+}
